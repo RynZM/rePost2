b0d0e7a65700e0a85f824be8536021bcdbfeb16c
RynZM <Ryan.Z.McFarland@gmail.com>
2023-03-09 20:09:36 -0700

    itWorks

--- intro.py
+++ intro.py
@@ -1,9 +1,28 @@
-from PIL import Image
+# import cv2 
+# import pytesseract
+# pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
+# img = cv2.imread('C:\\users\\RyanZ\\OneDrive\\Documents\\Coding\\rePost\\Python\\rePost2\\image.jpg')
+
+# custom_config = r'--oem 3 --psm 6'
+# pytesseract.image_to_string(img, config=custom_config)
+
+
+
+
+from PIL import Image
 import pytesseract
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
+print(pytesseract.image_to_string(r'C:\\users\\RyanZ\\OneDrive\\Documents\\Coding\\rePost\\Python\\rePost2\\random.png'))
 
 
 
-print(pytesseract.image_to_string(Image.open('C:\\Users\\RyanZ\\OneDrive\\Documents\\Coding\\rePost\\Python\\rePost2\\image.jpg')))
+
+# import pytesseract
+# from PIL import Image
+
+
+# def print_text():
+#     print(pytesseract.image_to_string('r:\\users\\RyanZ\\OneDrive\\Documents\\Coding\\rePost\\Python\\rePost2\\random.png'))
